fc5cbeb6da33d9e7d6bcd667aa6e9b28bf81c53f
Mike Pridik <git@formsatz.de>
2024-07-17 13:27:00 +0200

    Gibt was aus :o)

--- app.js
+++ app.js
@@ -4,49 +4,61 @@ document.addEventListener('DOMContentLoaded', () => {
     // Let's go der Hase!
     console.info('%cmade with ♥ Mike Pridik', 'background: rgb(43,84,124); color:#fff;padding: 3px');
 
+    const readerWrapper = document.getElementById('reader');
+    const resultWrapper = document.getElementById('result');
+    const footer = document.getElementById('footer');
+
+    function onScanSuccess(decodedText, scanner) {
+        console.log(decodedText);
+        fetchFoodInfo(decodedText);
+        scanner.clear();
+        resultWrapper.innerHTML = `<a href="index.html" class="btn">Neuer Scannen</a>`;
+        readerWrapper.remove();
+    }
+
     if (typeof Html5QrcodeScanner !== 'undefined') {
-        const scanner = new Html5QrcodeScanner('reader', {
+        const html5QrcodeScanner = new Html5QrcodeScanner('reader', {
             qrbox: {
                 width: 250,
-                height: 250,
+                height: 250
             },
             fps: 20,
         });
-		
-        scanner.render(success, error);
-        function success(result){
-            document.getElementById('result').innerHTML = `
-			<h2>Scann erfolgreich!</h2>
-			<p><a href="${result}">${result}</a></p>
-			<button href="index.html">nochmal scannen</button>
-				`;
-            scanner.clear();
-		
-            document.getElementById('reader').remove();
-		
-        }
- 
-        function error(err){
-            // console.error(err);
-        }
+
+        html5QrcodeScanner.render((decodedText) => 
+            onScanSuccess(decodedText, html5QrcodeScanner)
+        );
     } else {
         console.error('Html5Qrcode ist nicht definiert. Stellen Sie sicher, dass die Bibliothek korrekt geladen wurde.');
     }
 
-    // Funktion zum Abrufen der Daten von Open Food Facts
-    function fetchFoodInfo(barcode) {
-        fetch(`https://world.openfoodfacts.org/api/v0/product/${barcode}.json`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 1) {
-                    var product = data.product;
-                    document.getElementById('foodInfo').innerText = `Produktname: ${product.product_name}\nMarke: ${product.brands}\nNährwert: ${product.nutriments['energy-kcal']} kcal`;
-                } else {
-                    document.getElementById('foodInfo').innerText = 'Produkt nicht gefunden.';
+    async function fetchFoodInfo(code) {
+        const url = `https://world.openfoodfacts.org/api/v0/product/${code}.json`;
+        try {
+            let response = await fetch(url);
+            if (!response.ok) {
+                if (response.status === 0) {
+                    console.log('Kein Code übergeben');
+                    resultWrapper.innerHTML = `<a href="index.html" class="btn">Neuer Scannen</a>`;
                 }
-            })
-            .catch(error => {
-                console.log('Fehler beim Abrufen der Daten: ', error);
-            });
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            let data = await response.json();
+            console.log('data: ', data);
+            const product = data.product;
+            resultWrapper.innerHTML = `
+                <span>Angaben zum Produkt</span>
+                <h2 class="txt--shadow">${product.product_name_de}</h2>
+                <img src="${product.selected_images.front.display.de}" alt="${product.product_name_de}">
+
+                
+            `;
+            footer.innerHTML = `<a href="index.html" class="btn">Neuer Scannen</a>`;
+            return data;
+
+        } catch (error) {
+            console.error('Fehler beim Abrufen der Nährstoffe:', error);
+            resultWrapper.innerHTML = `<a href="index.html" class="btn">Neuer Scan</a>`;
+        }
     }
 });
